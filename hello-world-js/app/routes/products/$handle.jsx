c6a46c9735bac5f5f94989fe535e7f1b1c2a7df3
David Witt <david.witt@shopify.com>
2023-01-26 09:42:03 -0800

    Updated Money objects, fixed a layout issue, added 404 logic

--- hello-world-js/app/routes/products/$handle.jsx
+++ hello-world-js/app/routes/products/$handle.jsx
@@ -22,6 +22,10 @@ export const loader = async ({params, context, request}) => {
 
   const {product} = data;
 
+  if (!product?.id) {
+    throw new Response(null, {status: 404});
+  }
+
   // optionally set a default variant so you always have an "orderable" product selected
   const selectedVariant =
     product.selectedVariant ?? product?.variants?.nodes[0];
@@ -40,7 +44,7 @@ export default function ProductHandle() {
   return (
     <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
       <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
-        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2">
+        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
           <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
             <ProductGallery media={product.media.nodes} />
           </div>
@@ -60,7 +64,7 @@ export default function ProductHandle() {
           />
           <Money
             withoutTrailingZeros
-            data={selectedVariant.price}
+            data={selectedVariant.priceV2}
             className="text-xl font-semibold mb-2"
           />
           {orderable && (
@@ -97,7 +101,7 @@ function ProductGallery({media}) {
 
   return (
     <div
-      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
+      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-[90vw] md:w-full lg:col-span-2`}
     >
       {media.map((med, i) => {
         let extraProps = {};
@@ -214,7 +218,7 @@ const PRODUCT_QUERY = `#graphql
           width
           height
         }
-        price {
+        priceV2 {
           amount
           currencyCode
         }
@@ -238,7 +242,7 @@ const PRODUCT_QUERY = `#graphql
           id
           title
           availableForSale
-          price {
+          priceV2 {
             currencyCode
             amount
           }
